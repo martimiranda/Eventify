3b0f8fb1c53bd6527a14fe7f723ce468c6ca3a8a
Tianle <tianleyin8888@gmail.com>
2024-03-18 20:08:58 +0100

    18/03

--- www/js/init.js
+++ www/js/init.js
@@ -24,11 +24,37 @@ document.addEventListener('DOMContentLoaded', function () {
     var tabs = document.querySelectorAll('.tabs');
     M.Tabs.init(tabs);
 
-
     tabs.forEach(function (tab) {
         tab.addEventListener('shown', function () {
             M.updateTextFields(); // Actualizar los campos de texto si es necesario
             M.AutoInit(); // Volver a inicializar los elementos de Materialize en la pestaña activa
         });
     });
+
+
+    var premiumLink = document.getElementById('PremiumApp');
+    var backPremium = document.getElementById('backPremium');
+    var premiumPage = document.getElementById('mainPremium');
+    var mainPage = document.getElementById('main1');
+    var footer = document.getElementById('footer');
+    var header = document.getElementById('header');
+
+    premiumLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+
+        premiumPage.style.display = "block";
+        mainPage.style.display = "none";
+        footer.style.pointerEvents = "none"; // Bloquear eventos en el footer
+        header.style.pointerEvents = "none"; // Bloquear eventos en el header    
+    });
+
+    backPremium.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        premiumPage.style.display = "none";
+        mainPage.style.display = "block";
+        footer.style.pointerEvents = "auto";
+        header.style.pointerEvents = "auto"; 
+    })
+
 })
